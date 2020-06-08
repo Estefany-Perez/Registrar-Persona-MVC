72994f0db37e4d83217c74859679d13aaef17dad
BrayanEduardo <bf377954@gmail.com>
2020-06-07 22:06:39 -0600

    Modificando Controlador

--- src/java/controlador/Editar_B.java
+++ src/java/controlador/Editar_B.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+//Brayan Eduardo Flores Rodriguez
 package controlador;
 
 import java.io.IOException;

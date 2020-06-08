abb1df97882ec822c75286c3e11c1290e394ab7f
BrayanEduardo <bf377954@gmail.com>
2020-06-07 22:34:10 -0600

    Modificando el Controlador Editar_B

--- src/java/controlador/Editar_B.java
+++ src/java/controlador/Editar_B.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//Brayan Eduardo Flores Rodriguez
 package controlador;
 
 import java.io.IOException;
